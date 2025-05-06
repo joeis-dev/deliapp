dc88450c79eca13c7b8be9e5d307a333f5d206fe
Jose Garcia <josegarcia.dev@gmail.com>
2025-05-05 22:54:32 -0600

    refactor: Backend folder re-structured

--- backend/listings/ServiceListingsApp.java
+++ backend/listings/ServiceListingsApp.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServiceListingsApplication {
-
+public class ServiceListingsApp {
   public static void main(String[] args) {
-      SpringApplication.run(ServiceListingsApplication.class, args);
+      SpringApplication.run(ServiceListingsApp.class, args);
   }
 }
